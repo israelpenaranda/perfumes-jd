8c5f751a0784e024099a89832a36593bf265ae97
Israel Peñaranda <josuepenaranda1303@gmail.com>
2026-06-10 15:19:23 -0400

    disable cache

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,9 @@ import CartButton from "./components/CartButton";
 import ProductList from "./components/ProductList";
 import { supabase } from "./lib/supabase";
 
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
 export default async function Home() {
   const { data: products } = await supabase
     .from("products")
